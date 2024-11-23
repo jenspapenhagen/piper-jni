b3ffc0b8c6e91c885faeb12fe6d48649a26caf78
jenspapenhagen <jens.papenhagen@web.de>
2024-11-23 10:52:09 +0100

    first steps cleanup. only final fields as methode parameter

--- internal/NativeUtils.java
+++ internal/NativeUtils.java
@@ -40,7 +40,6 @@ import java.nio.file.StandardCopyOption;
  *
  * @see <a href="http://adamheinrich.com/blog/2012/how-to-load-native-jni-library-from-jar">http://adamheinrich.com/blog/2012/how-to-load-native-jni-library-from-jar</a>
  * @see <a href="https://github.com/adamheinrich/native-utils">https://github.com/adamheinrich/native-utils</a>
- *
  */
 public class NativeUtils {
 
@@ -63,19 +62,19 @@ public class NativeUtils {
 
     /**
      * Loads library from current JAR archive
-     *
+     * <p>
      * The file from JAR is copied into system temporary directory and then loaded. The temporary file is deleted after
      * exiting.
      * Method uses String as filename because the pathname is "abstract", not system-dependent.
      *
      * @param path The path of file inside JAR as absolute path (beginning with '/'), e.g. /package/File.ext
-     * @throws IOException If temporary file creation or read/write operation fails
+     * @throws IOException              If temporary file creation or read/write operation fails
      * @throws IllegalArgumentException If source file (param path) does not exist
      * @throws IllegalArgumentException If the path is not absolute or if the filename is shorter than three characters
-     * (restriction of {@link File#createTempFile(java.lang.String, java.lang.String)}).
-     * @throws FileNotFoundException If the file could not be found inside the JAR.
+     *                                  (restriction of {@link File#createTempFile(java.lang.String, java.lang.String)}).
+     * @throws FileNotFoundException    If the file could not be found inside the JAR.
      */
-    public static void loadLibraryFromJar(String path) throws IOException {
+    public static void loadLibraryFromJar(final String path) throws IOException {
 
         if (null == path || !path.startsWith("/")) {
             throw new IllegalArgumentException("The path has to be absolute (start with '/').");
@@ -127,13 +126,13 @@ public class NativeUtils {
                     .supportedFileAttributeViews()
                     .contains("posix");
         } catch (FileSystemNotFoundException
-                | ProviderNotFoundException
-                | SecurityException e) {
+                 | ProviderNotFoundException
+                 | SecurityException e) {
             return false;
         }
     }
 
-    private static File createTempDirectory(String prefix) throws IOException {
+    private static File createTempDirectory(final String prefix) throws IOException {
         String tempDir = System.getProperty("java.io.tmpdir");
         File generatedDir = new File(tempDir, prefix + System.nanoTime());
 
